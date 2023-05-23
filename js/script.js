addc6909dc19ba6e233c8d4bb0bfb8a2eda3a3e0
giovannilampis <giolampis@hotmail.com>
2023-05-23 16:04:32 +0200

    add callApi() to methods in script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@ createApp({
       return {
         message: 'Hello Vue!',
         apiUrl: 'server.php',
+        data: '',
       }
     },
     methods: {
-      
+      callApi() {
+        axios.get( this.apiUrl ).then( (res) => {
+          this.data = res.data;
+        } )
+      }
     },
   }).mount('#app')
